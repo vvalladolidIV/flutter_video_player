ce9153513b9239042cca6cb233129106652880ab
vvalladolid <vvalladolid@isvisoft.com>
2020-01-23 13:46:10 +0100

    clean

--- test/video_player_test.dart
+++ test/video_player_test.dart
@@ -7,8 +7,8 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
-import 'package:video_player/video_player.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:video_player/video_player.dart';
 import 'package:video_player_platform_interface/video_player_platform_interface.dart';
 
 class FakeController extends ValueNotifier<VideoPlayerValue>

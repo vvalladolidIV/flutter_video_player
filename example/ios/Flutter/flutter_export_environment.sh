791947e9b04eba5caef84f94768711b69769a8eb
vvalladolid <vvalladolid@isvisoft.com>
2020-01-23 13:27:25 +0100

    firs commit

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=D:\Archivos de programa\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\vvall\Desktop\flutter_video_player\example"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=D:\Archivos de programa\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
